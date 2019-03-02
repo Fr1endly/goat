42246f2bd4b49d02628f009b279cea589cda840c
Fr1endly <GizatulinsO@gmail.com>
2019-03-03 01:44:09 +0200

    Rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,7 +6,7 @@ class ItemValidationTest(FunctionalTest):
     #Edith goes to home page and accidently submits empty list item by pressing enter 
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
     #Home page refreshes 
         self.wait_for(lambda: self.assertEqual(
@@ -14,15 +14,15 @@ class ItemValidationTest(FunctionalTest):
             "You can't have an empty list item"
         ))
     #She tries with some text now, which works
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
     #And then again accidently send blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, 
         "You can't have an empty list item"))
         #And now she corrects it by entering some items
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')  
